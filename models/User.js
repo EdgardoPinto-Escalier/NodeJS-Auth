186744deb5ec00028f4c346bc504c4a72e9c09fd
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-09-16 20:46:30 +0200

    Updated the login method

--- models/User.js
+++ models/User.js
@@ -17,13 +17,25 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-
 // Fire a function before the new user is saved to the DB
 userSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
-})
+});
+
+// Static method to login users
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('Incorrect password');
+  }
+  throw Error('Incorrect email');
+}
 
 
 
