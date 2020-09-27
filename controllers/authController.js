19ef82ce93ff611cbbe04d93e5ccdf9189439d74
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-09-27 17:20:48 +0200

    Added new functionality to logout users

--- controllers/authController.js
+++ controllers/authController.js
@@ -78,3 +78,9 @@ module.exports.login_post = async (req, res) => {
     res.status(400).json({ errors });
   }
 };
+
+// Logout users
+module.exports.logout_get = (req, res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/');
+}
